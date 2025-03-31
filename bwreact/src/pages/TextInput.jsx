dffe84bdbe25afe13290a671d5e28360feb82d7c
donjoo <donjorois@gmail.com>
2025-03-31 22:01:33 +0530

    react

--- bwreact/src/pages/TextInput.jsx
+++ bwreact/src/pages/TextInput.jsx
@@ -1,8 +1,14 @@
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 const TextInput = () => {
     const inputRef = useRef(null)
+    const renders = useRef(1)
+    const [count, setCount] = useState(0);
 
+
+    renders.current += 1
+    console.log('renderinggg')
+    console.log('jj')
     const focusInput = () => {
         inputRef.current.focus()
     }
@@ -11,6 +17,12 @@ const TextInput = () => {
         <>
         <input ref = {inputRef} type="text"></input>
         <button onClick={focusInput}>focusInput</button>
+
+        <div>
+            <p>Render Count: {count} </p>
+        <p>Componet re-rendered: {renders.current} times</p>
+        <button onClick = {() => setCount(count + 1)}>Increase COunt</button>
+        </div>
         </>
     )
 }
