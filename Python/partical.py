6948ea28dca12a1b5ad2ec55759c383351e4d86d
donjoo <donjorois@gmail.com>
2025-09-10 22:24:48 +0530

    commit before starting system design

--- Python/partical.py
+++ Python/partical.py
@@ -1,33 +1,38 @@
-import requests
+# import requests
 
-# Step 1: Provide your URL
-url = "https://api.github.com/users/octocat"
+# # Step 1: Provide your URL
+# url = "https://api.github.com/users/octocat"
 
-# Step 2: Send GET request to the URL
-response = requests.get(url)
+# # Step 2: Send GET request to the URL
+# response = requests.get(url)
 
-# Step 3: Parse JSON into Python dict
-data = response.json()
+# # Step 3: Parse JSON into Python dict
+# data = response.json()
 
-# Step 4: Use the data
-print(data["login"])       # ➝ 'octocat'
-print(data["public_repos"])  # ➝ e.g., 8
+# # Step 4: Use the data
+# print(data["login"])       # ➝ 'octocat'
+# print(data["public_repos"])  # ➝ e.g., 8
 
 
-import random
+# import random
 
-def unique_random(jees):
-    items = list(jees)
-    while items:
-        choice = random.choice(items)
-        items.remove(choice)
-        yield choice
-    raise StopIteration('no more unique elements remaining')
+# def unique_random(jees):
+#     items = list(jees)
+#     while items:
+#         choice = random.choice(items)
+#         items.remove(choice)
+#         yield choice
+#     raise StopIteration('no more unique elements remaining')
     
-jees = [1,'fdd',False,34,'gg','daf',0]
-try:
+# jees = [1,'fdd',False,34,'gg','daf',0]
+# try:
     
     
-res = unique_random(jees)
-for i in res:
-    print(i)
+# res = unique_random(jees)
+# for i in res:
+#     print(i)
+
+
+import subprocess
+
+subprocess.run(["ls"])
